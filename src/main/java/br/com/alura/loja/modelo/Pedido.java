4d25b335d9c2e5f2d0474372826ee52d5aadaaaa
julianomachadoo <julmachadoramos@gmail.com>
2022-12-27 09:58:53 -0300

    "ItemPedido e mapeamento many-to-many"

--- src/main/java/br/com/alura/loja/modelo/Pedido.java
+++ src/main/java/br/com/alura/loja/modelo/Pedido.java
@@ -3,24 +3,28 @@ package br.com.alura.loja.modelo;
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Table(name = "pedidos")
-public class Pedidos {
+public class Pedido {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private BigDecimal valorTotal;
-    private LocalDate data = LocalDate.now();
+    private final LocalDate data = LocalDate.now();
 
     @ManyToOne
     private Cliente cliente;
 
-    public Pedidos() {
+    @OneToMany
+    private List<ItemPedido> pedidos;
+
+    public Pedido() {
     }
 
-    public Pedidos(Cliente cliente) {
+    public Pedido(Cliente cliente) {
         this.cliente = cliente;
     }
 }
