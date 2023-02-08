7fcbeeaa0d0ab728c52c71fc516d222cc3f2fc1e
julianomachadoo <julmachadoramos@gmail.com>
2023-02-08 17:00:41 -0300

    consultas com funções de agregação

--- src/main/java/br/com/alura/loja/dao/PedidoDao.java
+++ src/main/java/br/com/alura/loja/dao/PedidoDao.java
@@ -8,14 +8,19 @@ import java.util.List;
 
 public class PedidoDao {
 
-	private EntityManager em;
+    private EntityManager em;
 
-	public PedidoDao(EntityManager em) {
-		this.em = em;
+    public PedidoDao(EntityManager em) {
+        this.em = em;
+    }
+
+    public void cadastrar(Pedido pedido) {
+        this.em.persist(pedido);
+    }
+
+    public BigDecimal valorTotalVendido() {
+		String jpql = "SELECT SUM(p.valorTotal) FROM Pedido p";
+		return em.createQuery(jpql, BigDecimal.class)
+				.getSingleResult();
 	}
-
-	public void cadastrar(Pedido pedido) {
-		this.em.persist(pedido);
-	}
-
 }
--- src/main/java/br/com/alura/loja/testes/CadastroDePedido.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDePedido.java
@@ -29,6 +29,9 @@ public class CadastroDePedido {
         pedidoDao.cadastrar(pedido);
 
         em.getTransaction().commit();
+
+        BigDecimal totalVendido = pedidoDao.valorTotalVendido();
+        System.out.println("VALOR TOTAL: " + totalVendido);
     }
 
     private static void popularBancoDeDados() {
