eb04d7ebfe2eb7381cc67b66e0a9db50cb948e56
julianomachadoo <julmachadoramos@gmail.com>
2023-02-11 16:23:05 -0300

    Mapeamento de herança com SingleTable

--- src/main/java/br/com/alura/loja/modelo/Produto.java
+++ src/main/java/br/com/alura/loja/modelo/Produto.java
@@ -8,75 +8,76 @@ import javax.persistence.*;
 @Entity
 @Table(name = "produtos")
 @NamedQuery(name = "Produto.produtosPorCategoria",
-		query = "SELECT p FROM Produto p WHERE p.categoria.nome = :nome")
+        query = "SELECT p FROM Produto p WHERE p.categoria.nome = :nome")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public class Produto {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private String nome;
-	private String descricao;
-	private BigDecimal preco;
-	private LocalDate dataCadastro = LocalDate.now();
-	@ManyToOne(fetch = FetchType.LAZY)
-	private Categoria categoria;
-	
-	public Produto() {
-	}
-	
-	public Produto(String nome, String descricao, BigDecimal preco, Categoria categoria) {
-		this.nome = nome;
-		this.descricao = descricao;
-		this.preco = preco;
-		this.categoria = categoria;
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private BigDecimal preco;
+    private LocalDate dataCadastro = LocalDate.now();
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Categoria categoria;
 
-	public Long getId() {
-		return id;
-	}
+    public Produto() {
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public Produto(String nome, String descricao, BigDecimal preco, Categoria categoria) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.categoria = categoria;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getDescricao() {
-		return descricao;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public BigDecimal getPreco() {
-		return preco;
-	}
+    public String getDescricao() {
+        return descricao;
+    }
 
-	public void setPreco(BigDecimal preco) {
-		this.preco = preco;
-	}
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 
-	public LocalDate getDataCadastro() {
-		return dataCadastro;
-	}
+    public BigDecimal getPreco() {
+        return preco;
+    }
 
-	public void setDataCadastro(LocalDate dataCadastro) {
-		this.dataCadastro = dataCadastro;
-	}
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
 
-	public Categoria getCategoria() {
-		return categoria;
-	}
+    public LocalDate getDataCadastro() {
+        return dataCadastro;
+    }
 
-	public void setCategoria(Categoria categoria) {
-		this.categoria = categoria;
-	}
+    public void setDataCadastro(LocalDate dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
 
 }
